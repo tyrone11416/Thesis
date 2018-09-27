9b0f295f29654b94549bdd51c7dd892bdc1445a2
tyrone11416 <34398477+tyrone11416@users.noreply.github.com>
2018-09-27 10:21:44 -0400

    Add files via upload

---
+++ Region_County_Example.py
@@ -0,0 +1,93 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import csv
+
+
+mFileName = "Region_and_County_Example.csv"
+
+G = nx.Graph()
+counter = 0
+
+
+def VisualizeGraph():
+    nx.draw(G, center=1581, node_size=300, node_color='blue', with_labels=True)
+    plt.savefig('region_county_network.png')
+    plt.show()
+
+
+def LoadData(file):
+    global counter
+    with open(file, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+
+
+        state=""
+        oldstate=""
+        state_counter=0
+        county_counter=0
+        region_counter=0
+        local_counter=0
+
+        region=""
+        oldregion=""
+
+        county=""
+        oldcounty=""
+
+        local=""
+        oldlocal=""
+
+        for row in reader:
+
+            #Print the data (for debugging purposes - comment out.)
+            print(row['state'], row['station_name'], row['county'], row['local'])
+
+            #This is quick and dirty just to prove you can do it.
+            state = row['state']
+
+            #state Level.
+            # if its a new state make a new top node.
+            if state != oldstate:
+                G.add_node(counter, weight=0.4, time='2pm', name=state)
+                G.add_edge(0, counter)
+                print(G.nodes[counter]) #print out node attributes to console.
+                state_counter = counter
+                counter = counter + 1
+                oldstate=state
+
+            region = row['region']
+            if region != oldregion:
+                G.add_node(counter, weight=0.4, time='2pm', name=region)
+                G.add_edge(state_counter, counter)
+                region_counter = counter
+                counter = counter + 1
+                oldregion=region
+
+            county = row['county']
+            if county != oldcounty:
+                G.add_node(counter, weight=0.4, time='2pm', name=county)
+                G.add_edge(region_counter, counter)
+                county_counter = counter
+                counter = counter + 1
+                oldcounty=county
+
+
+            #local Level
+            G.add_node(counter, weight=0.7, time='12pm', name=row['local'])
+            print(G.nodes[counter]) #print out node attributes to console.
+            G.add_edge(county_counter, counter, weight=4.7 )
+            local_counter = counter
+            counter = counter + 1
+
+def main():
+     global counter
+     G.add_node(counter, time='1pm', name="NDEx")
+     counter = counter + 1
+
+     print("Loading Data from file.... ", mFileName)
+     LoadData(mFileName)
+     VisualizeGraph()
+
+
+if __name__== "__main__":
+    main()
